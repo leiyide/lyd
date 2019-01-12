ca134edce7b83cd8cc9f04e3db5f56e3d9c247d9
leiyide <1026938865@qq.com>
2019-01-12 18:37:30 +0800

    modify

--- js/music.js
+++ js/music.js
@@ -2,14 +2,24 @@
  * Created by Kay on 2016/4/15.
  */
 // 自定义背景音乐的 播放/暂停 功能
+
+function repeat(){
+    var music = document.getElementById("music");
+	music.currentTime = 0;//重新播放
+	aTitle.title = "暂停音乐";
+}
+
 function playPause(){
 	var music = document.getElementById("music");
     var aTitle = document.getElementById("aTitle");
     if (music.paused){
         music.play();
         aTitle.title = "暂停音乐";
+    } else if (music.currentTime != 0){
+    	repeat();
     } else{
-        music.stop;
+        music.pause();
+        music.currentTime = 0
         aTitle.title = "播放音乐";
     }
 }
